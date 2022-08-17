789936bb47c1e64753212169d655ebc40754b011
Fabricio Moreira <fs_moreira@yahoo.com.br>
2022-08-16 22:26:49 -0300

    Desenvolvimento da pagina

--- contato.php
+++ contato.php
@@ -1,33 +1,38 @@
 <?php
-  ini_set('display_errors', 1);
-  error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-  $from = "suporte@conexbr.link";
-  //$to = "estiprojetos@estiprojetos.com.br";
-  $to = "fabricio@conexbr.link";
-  $phone = $_POST["telefone"];
+    $from = "suporte@conexbr.link";
+    //$to = "estiprojetos@estiprojetos.com.br";
+    $to = "fabricio@conexbr.link";
+    $phone = $_POST["telefone"];
 
-  $subject = "Contato pelo site: " . $_POST["nomeContato"];
-  $message = "Nome.....: " . $_POST["nomeContato"] . "\r\n" .
-             "Empresa..: " . $_POST["nomeEmpresa"] . "\r\n" .
-             "Email....: " . $_POST["enderecoEmail"] . "\r\n" .
-             "Fone.....: " . $_POST["telefone"] . "\r\n" .
-             "Mensagem.: " . $_POST["mensagem"];
-  
-  $headers = "De:". $from;
+    $subject = "Contato pelo site: " . $_POST["nomeContato"];
+    $message = "Nome.....: " . $_POST["nomeContato"] . "\r\n" .
+                "Empresa..: " . $_POST["nomeEmpresa"] . "\r\n" .
+                "Email....: " . $_POST["enderecoEmail"] . "\r\n" .
+                "Fone.....: " . $_POST["telefone"] . "\r\n" .
+                "Mensagem.: " . $_POST["mensagem"];
+    
+    $headers = "De:". $from;
 
-  if (is_numeric($phone)) {
-    if ($to != null && $subject != null && $headers != null && $message != null){
-      mail($to, $subject, $message, $headers);
-      //mail($to2, $subject, $message, $headers);
+    if (is_numeric($phone)) {
+        if ($to != null && $subject != null && $headers != null && $message != null){
+            mail($to, $subject, $message, $headers);
+            //mail($to2, $subject, $message, $headers);
+        }
     }
-  }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<title>Enviando...</title>
-<meta http-equiv="REFRESH" content="1;url=/contato.html"></HEAD>
-<BODY>
-</BODY>
-</HTML>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Enviando...</title>
+        <!--<meta http-equiv="REFRESH" content="1;url=/contato.html"></HEAD>-->
+    </head>
+    <body>
+        <?php
+            $post_data = file_get_contents('php://input');
+            echo "<div> POST BODY <br>".$post_data."</div>"; 
+        ?>        
+    </body>
+</html>
