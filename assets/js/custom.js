3c3ea8173415739242d374b7c2f61d686c4fea60
Fabricio Moreira <fs_moreira@yahoo.com.br>
2022-08-17 07:40:11 -0300

    Desenvolvimento da pagina

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
    document.getElementById("buttonEnviar").disabled = false;
  }
 
-$('input[name="p_fone"]').keypress(function() {
+$('input[name="telefone"]').keypress(function() {
     if (this.value.length > 11) {
         return false;
     }
